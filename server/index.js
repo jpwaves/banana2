0f46d2a637ef2130c117cccdce1d7ecd7fb4b5e5
Justin Pong <jp.waves51@gmail.com>
2021-12-03 21:20:52 -0500

    login and register works and connects with db

--- server/index.js
+++ server/index.js
@@ -1,35 +1,57 @@
-const express = require('express')
-const app = express()
-const mysql = require('mysql2')
-const cors = require('cors')
+import express from "express";
+import mysql from "mysql2/promise";
+import cors from "cors";
+import * as dotenv from "dotenv";
 
+dotenv.config();
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: 'kitty2328', // REPLACE THIS TO WORK
-    database: 'banana',
+const db = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
 });
 
-app.post('/create', (req, res) => {
-    const name = req.body.username;
-    const pass = req.body.password;
-    const role = "user";
+app.post("/checkCreds", async (req, res) => {
+  const query = "SELECT true FROM appUser WHERE username = ? AND passwd = ?";
+  await db
+    .execute(query, [req.body.username, req.body.password])
+    .then(([data]) => {
+      if (data.length > 0) {
+        res.send({ result: true });
+      } else {
+        res.send({ result: false });
+      }
+    })
+    .catch((err) => {
+      throw err;
+    });
 
-    db.query(
-        'INSERT INTO appuser (username, passwd, role) VALUES (?, ?, ?)', 
-        [name, pass, role],
-        function(err, results) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.send("Registered");
-            }
-        });
+  // if username, passwd combo exist in appuser, it'll return a value so res.length == 1
+  // if it doesn't exist, it'll return nothing so res.length == 0
+});
+
+app.post("/create", async (req, res) => {
+  const name = req.body.username;
+  const pass = req.body.password;
+  const role = "user";
+
+  await db.query(
+    "INSERT INTO appuser (username, passwd, role) VALUES (?, ?, ?)",
+    [name, pass, role],
+    function (err, results) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Registered");
+      }
+    }
+  );
 });
 
 app.listen(3001, () => {
-    console.log("yay");
+  console.log("yay");
 });
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,71 +1,131 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 import "./Login.scss";
-import Axios from 'axios';
-import { checkCredentials } from '../../middleware';
+import Axios from "axios";
 
 export default function Login() {
+  const [username, setName] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [username, setName] = useState("");
-    const [password, setPassword] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [loginUsername, setLoginName] = useState("");
 
-    const [loginPassword, setLoginPassword] = useState("");
-    const [loginUsername, setLoginName] = useState("");
-
-    const [loginStatus, setLoginStatus] = useState("");
-
-    const addUser = () => {
-        Axios.post('http://localhost:3001/create', {
-                username: username,
-                password: password,
-            }).then(() => {
-                console.log("success");
-        });
-    };
-
-    const login = () => {
-        setLoginStatus(checkCredentials(loginUsername, loginPassword));
-        console.log(loginStatus);
+  const [loginStatus, setLoginStatus] = useState("none");
+  // check if login status was update correctly (firstUpdate to prevent useEffect from execing on page load)
+  const firstUpdate = useRef(true);
+  useEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
     }
+    console.log(loginStatus);
+  }, [loginStatus]);
 
-    return(
-        <div className="login-wrapper">
-        <form id = "exist">
+  const addUser = () => {
+    Axios.post("http://localhost:3001/create", {
+      username: username,
+      password: password,
+    }).then(() => {
+      console.log("success");
+    });
+  };
+
+  const loginUsernameField = useRef(null);
+  const loginPasswordField = useRef(null);
+  const clearLoginTxtFields = () => {
+    loginUsernameField.current.value = "";
+    loginPasswordField.current.value = "";
+  };
+
+  const registerUsernameField = useRef(null);
+  const registerPasswordField = useRef(null);
+  const clearRegisterTxtFields = () => {
+    registerUsernameField.current.value = "";
+    registerPasswordField.current.value = "";
+  };
+  const login = () => {
+    Axios.post("http://localhost:3001/checkCreds", {
+      username: loginUsername,
+      password: loginPassword,
+    })
+      .then((res) => {
+        setLoginStatus(res.data.result);
+        clearLoginTxtFields();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="login-wrapper">
+      <form id="exist">
         <h2>Log In</h2>
-          <label for="name"> Username </label>
-            <input type="text" id = "name"
-            onChange={
-                (event) => {setLoginName(event.target.value)}
-            } />
+        <label for="name"> Username </label>
+        <input
+          ref={loginUsernameField}
+          type="text"
+          id="name"
+          onChange={(event) => {
+            setLoginName(event.target.value);
+          }}
+        />
 
-          <label for="pass"> Password </label>
-            <input type="text" id = "pass"
-            onChange={
-                (event) => {setLoginPassword(event.target.value)}
-            } />
-          <div>
-            <button onClick={() => {login()}} type="submit" id = "submit-login">Log In</button>
-          </div>
-        </form>
+        <label for="pass"> Password </label>
+        <input
+          ref={loginPasswordField}
+          type="text"
+          id="pass"
+          onChange={(event) => {
+            setLoginPassword(event.target.value);
+          }}
+        />
+        <div>
+          <button
+            onClick={() => {
+              login();
+            }}
+            type="button"
+            id="submit-login"
+          >
+            Log In
+          </button>
+        </div>
+      </form>
 
-        <form id = "register">
+      <form id="register">
+        <h2> Register </h2>
+        <label for="name"> Username </label>
+        <input
+          ref={registerUsernameField}
+          type="text"
+          id="name"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
 
-            <h2> Register </h2>
-          <label for="name"> Username </label>
-            <input type="text" id = "name" 
-            onChange={
-                (event) => {setName(event.target.value)}
-            } />
-
-          <label for="pass"> Password </label>
-            <input type="text" id = "pass" 
-            onChange={
-                (event) => {setPassword(event.target.value)}
-            }/>
-          <div>
-            <button onClick={() => {addUser()}} type="submit" id = "submit-register"
-            >Register</button>
-          </div>
-        </form>
-      </div>
-    );
+        <label for="pass"> Password </label>
+        <input
+          ref={registerPasswordField}
+          type="text"
+          id="pass"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        <div>
+          <button
+            onClick={() => {
+              addUser();
+              clearRegisterTxtFields();
+            }}
+            type="button"
+            id="submit-register"
+          >
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
