ed63b20e38249ce3a4221377aaae959afe3be6a8
Justin Pong <jp.waves51@gmail.com>
2021-12-05 16:40:16 -0500

    Init meme page component

---
+++ src/components/memePage/MemePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function MemePage() {
+  return (
+    <div className="container">
+      <h2>title</h2>
+      <p>description</p>
+      <h3>Memes</h3>
+      <ul>
+        <li>nice</li>
+      </ul>
+    </div>
+  );
+}
